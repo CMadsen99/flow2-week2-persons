7310a507d875a2325bfbc4457bba2cc681d06725
CMadsen99 <cph-cm298@cphbusiness.dk>
2020-10-02 14:58:18 +0200

    first

---
+++ src/main/java/dto/PersonsDTO.java
@@ -0,0 +1,30 @@
+
+package dto;
+
+import entities.Person;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Acer
+ */
+public class PersonsDTO {
+    
+    List<PersonDTO> all = new ArrayList();
+    
+    public PersonsDTO (List<Person> personEntities) {
+        personEntities.forEach((p) -> {
+            all.add(new PersonDTO(p));
+        });
+    }
+    
+    public int size() {
+        return all.size();
+    }
+    
+    public List<PersonDTO> getAll() {
+        return all;
+    }
+    
+}
